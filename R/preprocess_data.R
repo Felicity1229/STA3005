5763cde799217b97742d083912f0933ea96f98af
ZaOchuAN-08 <123090837@link.cuhk.edu.cn>
2026-04-07 20:02:17 +0800

    data_process_updated 4.0

--- R/preprocess_data.R
+++ R/preprocess_data.R
@@ -1,37 +1,53 @@
 preprocess_data <- function(file_path, tag_column, split_ratio = 0.7, seed = 3) {
   # 1 读取数据----
   df <- read.csv(file_path)
-  # head(df)
-  # str(df)
-  # summary(df)
+  head(df)
+  str(df)
+  summary(df)
+  tag_col_name <- names(df)[tag_column]
 
   # 2 检查缺失值情况----
-  # colSums(is.na(df))
+  missing_counts <- colSums(is.na(df))
+  missing_counts
 
   # 3 用均值填充缺失值----
+  if (sum(missing_counts) > 0){
+    cols_with_na <- names(missing_counts[missing_counts > 0])
+    cols_with_na
 
-  # ph
-  df$ph[is.na(df$ph)] <- mean(df$ph, na.rm = TRUE)
-  # Sulfate
-  df$Sulfate[is.na(df$Sulfate)] <- mean(df$Sulfate, na.rm = TRUE)
-  # Trihalomethanes
-  df$Trihalomethanes[is.na(df$Trihalomethanes)] <- mean(df$Trihalomethanes, na.rm = TRUE)
+    for (col in cols_with_na) {
+      df[[col]][is.na(df[[col]])] <- mean(df[[col]], na.rm = TRUE)
+    }
 
-  colSums(is.na(df))
+    print("Filled:")
+    print(paste(cols_with_na, collapse = ", "))
+    print(colSums(is.na(df)))
+  }else{
+    print("No missing value")
+  }
 
   # 4 相关性分析-----
-  cor_matrix <- cor(df)
-  print(cor_matrix)
+  numeric_cols <- sapply(df, is.numeric)
+
+  if (sum(numeric_cols) >= 2) {  # 至少需要2列才能计算相关性
+    cor_matrix <- cor(df[, numeric_cols])
+    print(cor_matrix)
+  } else {
+    print("The correlation matrix cannot be calculated.")
+  }
 
   ##library(corrplot)
   ##corrplot(cor_matrix, method = "color", type = "upper", tl.cex = 0.8)
+
+  # 5 因变量分布----
+  print(table(df[[tag_col_name]]))
 
   # 6 划分训练集和测试集----
   library(caTools)
   set.seed(seed)
 
   # 按 70% 训练，30% 测试划分
-  split <- sample.split(df$Potability, SplitRatio = split_ratio)
+  split <- sample.split(df[[tag_col_name]], SplitRatio = split_ratio)
 
   train_data <- subset(df, split == TRUE)
   test_data  <- subset(df, split == FALSE)
@@ -39,27 +55,44 @@ preprocess_data <- function(file_path, tag_column, split_ratio = 0.7, seed = 3) 
   dim(train_data)
   dim(test_data)
 
-  # 7 存储训练集和测试集的特征和标签----
-  X_train <- train_data[, tag_column]   # 去掉 Potability
-  y_train <- train_data$Potability
+  unique(train_data[[tag_col_name]])
 
-  X_test <- test_data[, tag_column]
-  y_test <- test_data$Potability
+  # 7 存储训练集和测试集的特征和标签----
+  X_train <- train_data[, -tag_column]
+  y_train <- as.numeric(factor(train_data[[tag_col_name]])) - 1
+
+  X_test <- test_data[, -tag_column]
+  y_test  <- as.numeric(factor(test_data[[tag_col_name]])) - 1
 
   # 8 归一化函数----
-  min_vals <- sapply(X_train, min)
-  max_vals <- sapply(X_train, max)
+  numeric_cols_train <- sapply(X_train, is.numeric)
 
-  min_max_norm <- function(x) {
-    return((x - min(x)) / (max(x) - min(x)))
+  if (sum(numeric_cols_train) > 0) {
+    min_vals <- sapply(X_train[, numeric_cols_train], min)
+    max_vals <- sapply(X_train[, numeric_cols_train], max)
+
+    min_max_norm <- function(x) {
+      return((x - min(x)) / (max(x) - min(x)))
+    }
+
+    X_train_numeric <- as.data.frame(lapply(X_train[, numeric_cols_train], min_max_norm))
+    X_train_non_numeric <- X_train[, !numeric_cols_train, drop = FALSE]
+
+    X_train_norm <- cbind(X_train_numeric, X_train_non_numeric)
+
+    # 测试集用训练集的 min 和 max
+    X_test_numeric <- as.data.frame(mapply(function(x, min_val, max_val) {
+      (x - min_val) / (max_val - min_val)
+    }, X_test[, numeric_cols_train], min_vals, max_vals, SIMPLIFY = FALSE))
+
+    X_test_non_numeric <- X_test[, !numeric_cols_train, drop = FALSE]
+
+    X_test_norm <- cbind(X_test_numeric, X_test_non_numeric)
+  }else{
+    X_train_norm = X_train
+    X_test_norm = X_test
+    min_vals = max_vals = cor_matrix = NA
   }
-
-  X_train_norm <- as.data.frame(lapply(X_train, min_max_norm))
-
-  # 测试集用训练集的 min 和 max
-  X_test_norm <- as.data.frame(mapply(function(x, min_val, max_val) {
-    (x - min_val) / (max_val - min_val)
-  }, X_test, min_vals, max_vals))
 
   # 注释----
   # 读入原始数据，对数据结构与基本统计特征进行了初步分析。检查缺失值情况，对存在缺失的变量采用mean imputation
@@ -84,8 +117,8 @@ preprocess_data <- function(file_path, tag_column, split_ratio = 0.7, seed = 3) 
 }
 
 # 调用
-file_path = "water_potability.csv"
-result <- preprocess_data(file_path, tag_column = -10)
+file_path = "E:/Semester6_Year3Term2/STA3005/STA3005/R/mushrooms.csv"
+result <- preprocess_data(file_path, tag_column = 1)
 
 # 特征
 X_train <- result$X_train
@@ -93,3 +126,5 @@ X_test  <- result$X_test
 # 标签
 y_train <- result$y_train
 y_test  <- result$y_test
+
+y_train
