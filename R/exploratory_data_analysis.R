18fb2ad6265fd02b95d3e54855d538e2ab2f7d85
Mignon-Miniver <humignon661@gmail.com>
2026-05-04 21:20:32 +0800

    1

--- R/exploratory_data_analysis.R
+++ R/exploratory_data_analysis.R
@@ -114,7 +114,8 @@ exploratory_data_analysis <- function(data, target_name) {
   cat("\n[3] Descriptive Statistics & Outliers\n")
   stats_with_outliers <- cbind(eda_results$Descriptive_Statistics,
                                Outliers = eda_results$Outlier_Counts)
-  print(round(stats_with_outliers, 3))
+  num_cols <- sapply(stats_with_outliers, is.numeric)
+  print(round(stats_with_outliers[num_cols], 3))
 
   # 7.4 Group Means Analysis
   cat("\n[4] Feature Means Grouped By Target (", target_name, ")\n", sep = "")
